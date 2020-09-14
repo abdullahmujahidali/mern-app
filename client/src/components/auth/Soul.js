b3d87006c1839745b5a0274d839d37bc3a47edb1
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2020-09-14 21:09:36 +0500

    Added heroku dependency

--- client/src/components/auth/Soul.js
+++ client/src/components/auth/Soul.js
@@ -2,37 +2,145 @@ import React from "react";
 
 const Register = () =>{
  return(
-    <div class="profile-grid my-1">
-    <div class="profile-top bg-primary p-2">
+    <div class="profile-grid my-1 ">
+    <div class="profile-top bg-dark p-2">
           <img
             class="round-img my-1"
-            src="//www.gravatar.com/avatar/a31yrKHuMszGZLqEbwv52pXCw5fwvfJDZt7?s=200&r=pg&d=mm"
-            alt=""
+            src="https://i.ibb.co/P9ZyLRy/abd.png"
+            alt="my"
           />
           <h1 class="large">Abdullah Mujahid Ali</h1>
           <p class="lead">Developer at deSwot</p>
           <p>Lahore, PK</p>
           <div class="icons my-1">
-            <a href="#" target="_blank" rel="noopener noreferrer">
+            <a href="/" target="_blank" rel="noopener noreferrer">
               <i class="fas fa-globe fa-2x"></i>
             </a>
-            <a href="#" target="_blank" rel="noopener noreferrer">
+            <a href="https://twitter.com/abdulladgaf" target="_blank" rel="noopener noreferrer">
               <i class="fab fa-twitter fa-2x"></i>
             </a>
-            <a href="#" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.facebook.com/m.abdullahmujahid" target="_blank" rel="noopener noreferrer">
               <i class="fab fa-facebook fa-2x"></i>
             </a>
-            <a href="#" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/abdullah-mujahid-211849186/" target="_blank" rel="noopener noreferrer">
               <i class="fab fa-linkedin fa-2x"></i>
             </a>
-             <a href="#" target="_blank" rel="noopener noreferrer">
-              <i class="fab fa-youtube fa-2x"></i>
-            </a>
-            <a href="#" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/abdullahmujahidali/" target="_blank" rel="noopener noreferrer">
               <i class="fab fa-instagram fa-2x"></i>
             </a>
           </div>
         </div>
+        <div class="profile-about bg-dark p-2">
+          <h2 class="text-primary">Abdullah's Bio</h2>
+          <p>
+          I have a passion for learning and sharing my knowledge with others as publicly as possible. 
+If you found value in something I have created Feel free to reach out in case you want to just get in touch also.
+I am trying to become a full-stack developer. I am working at deSwot which provide web-services and moreover I am currently working on a project that is entitled with the company name "deSwot" which basically going to provide educational service.
+Other than that I am a writer. I have written books on Wattpad 😹
+
+          </p>
+          <div class="line"></div>
+          <h2 class="text-primary">Skill Set</h2>
+          <div class="skills">
+            <div class="p-1"><i class="fa fa-check"></i> JavaScript</div>
+            <div class="p-1"><i class="fa fa-check"></i> Python</div>
+            <div class="p-1"><i class="fa fa-check"></i> Matlab</div>
+            <div class="p-1"><i class="fa fa-check"></i> JAVA</div>
+            <div class="p-1"><i class="fa fa-check"></i> PHP</div>
+          </div>
+        </div>
+        <div class="profile-exp bg-dark p-2">
+          <h2 class="text-primary">Experience</h2>
+          <div>
+            <h3 class="text-light">Developer at deSwot</h3>
+            <p>Oct 2019 - Current</p>
+            <p><strong>Position: </strong>Full-Stack Developer</p>
+            <p>
+              <strong>Description: </strong>deSwot is a Lebanon based company which is on its horizon soon it will be start operating their services.
+              As a developer at deSwot my job is to handle the backend queries of the web application.
+            </p>
+          </div>
+         
+          <div>
+            <h3 class="text-light">Freelancer</h3>
+            <p>Oct 2017 - Current</p>
+            <p><strong>Position: </strong>Freelancer</p>
+            <p>
+              <strong>Description: </strong>Working as a freelancer from 2017 doing different types of projects related to Computer Science.
+              Other than that I also write in free time and offer my skills on the freelancing market and I have a very good reputation on many
+              fronts. 
+            </p>
+          </div>
+          <div>
+            <h3 class="text-light">Chegg</h3>
+            <p>June 2018 - March 2019</p>
+            <p><strong>Position: </strong>Solution Expert</p>
+            <p>
+              <strong>Description: </strong>In this short tenure I worked for Chegg.inc and helped students. My job was to answer the questions related
+              to my field of study Computer Science. It was a very fun experience but had to leave as other possibilities were opened. I used to solve C++, JAVA, Python,
+              Matlab, Image Processing and other Computer Science subjects.
+
+            </p>
+          </div>
+          <div>
+            <h3 class="text-light">CourseHero</h3>
+            <p>March 2019 - Current</p>
+            <p><strong>Position: </strong>Solution Expert</p>
+            <p>
+              <strong>Description: </strong>Following up Chegg.inc I moved towards another very great organization that is offering the same services. From March 2019
+              I am working as a solution expert at CourseHero and I am following up the same answering categorize that were of Chegg
+
+            </p>
+          </div>
+
+        </div>
+        <div class="profile-edu bg-dark p-2">
+          <h2 class="text-primary">Education</h2>
+          <div>
+            <h3>Cambridge University</h3>
+            <p>Sep 2013 - June 2015</p>
+            <p><strong>Degree: </strong>O'Levels</p>
+            <p><strong>Field Of Study: </strong>Computer Science</p>
+            <p>
+              <strong>Description: </strong>In 2015 I completed my Olevels.
+              My major was Computer Science and besides that I studied 8 others subjects.
+            </p>
+          </div>
+          <div>
+            <h3>Cambridge University</h3>
+            <p>Jan 2016 - Dec 2016</p>
+            <p><strong>Degree: </strong>A'levels</p>
+            <p><strong>Field Of Study: </strong>Computer Science</p>
+            <p>
+              <strong>Description: </strong>I completed my Alevels in 1 year. 
+              I jumped over different fields but in the end I completed my degree in semi-chemistry and in
+              semi-computer science.
+            </p>
+          </div>
+          <div>
+            <h3>University Of Management and Technology</h3>
+            <p>Jun 2017 - Current</p>
+            <p><strong>Degree: </strong>Bachelors</p>
+            <p><strong>Field Of Study: </strong>Computer Science</p>
+            <p>
+              <strong>Description: </strong> My first professional degree is about to complete.
+              My study major is Computer Science. The work field that I choose to carry on with is
+              Web Development and I am pursuing my carrer as a backend developer.
+            </p>
+          </div>
+          <div>
+            <h3>Certification</h3>
+            <p>Jun 2017 - Current</p>
+            <p><strong>Degree: </strong>Programming</p>
+            <p><strong>Field Of Study: </strong>Speed Programming</p>
+            <p>
+              <strong>Description: </strong> From 2017 I have taken part in many programming competition and I feel no shame
+              that I never one. My main goal of participating in such events was to learn and I did very much recently I took 
+              part in state ICPC competition and I was in the runner ups.
+            </p>
+          </div>
+        </div>
+
     </div>
  )
 }
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const conenction = require("./config/db");
 const  connectDB  = require("./config/db");
-
+const path=require("path");
 const app=express();
 
 //connect database
 connectDB();
-app.get("/",(req,res)=>res.send("API Running"));
+
 
 
 // init middleware
@@ -17,6 +17,19 @@ app.use('/api/users', require("./routes/api/users"));
 app.use('/api/auth', require("./routes/api/auth"));
 app.use('/api/profile', require("./routes/api/profile"));
 app.use('/api/posts', require("./routes/api/posts"));
+
+
+
+//serve static assets in production
+
+if(process.env.NODE_ENV=== "production"){
+    //set static folder
+    app.use(express.static("client/build"));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+    })
+}
+
 
 
 const PORT= process.env.PORT || 5000;
