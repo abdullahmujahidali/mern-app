916507e36b7090b75beff6424146401d2e1bdef0
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2020-09-14 22:22:04 +0500

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,37 +1,31 @@
-const express = require("express");
-const conenction = require("./config/db");
-const  connectDB  = require("./config/db");
-const path=require("path");
-const app=express();
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
-//connect database
+const app = express();
+
+// Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json());
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-// init middleware
-app.use(express.json({extended:false}))
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-//Define routes
-app.use('/api/users', require("./routes/api/users"));
-app.use('/api/auth', require("./routes/api/auth"));
-app.use('/api/profile', require("./routes/api/profile"));
-app.use('/api/posts', require("./routes/api/posts"));
-
-
-
-//serve static assets in production
-
-if(process.env.NODE_ENV=== "production"){
-    //set static folder
-    app.use(express.static("client/build"));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"client","build","index.html"));
-    })
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
+const PORT = process.env.PORT || 5000;
 
-
-const PORT= process.env.PORT || 5000;
-
-app.listen(PORT,()=>console.log("Server is up and running"));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
