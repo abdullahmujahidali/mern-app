10f1d5727eb9658d762a9a008fe7cad6088e935f
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2022-10-08 09:39:21 +0500

    added a log

--- server.js
+++ server.js
@@ -29,3 +29,7 @@ if (process.env.NODE_ENV === 'production') {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
